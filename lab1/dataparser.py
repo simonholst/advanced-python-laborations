14b7c75cbedb8f106b65a46a1d256d1699bc008b
Johan Selin <johanselin99@gmail.com>
2021-11-03 20:15:18 +0100

    Added a few TODOs as reminders and created datafiles in same directory as the parser to circumvent FileNotFound

--- lab1/dataparser.py
+++ lab1/dataparser.py
@@ -15,10 +15,12 @@ def build_tram_stops(jsonobject):
 def build_tram_lines(lines):
     tram_lines = lines.read()
     tram_lines = tram_lines.split("\n\n")
-    del tram_lines[-1]
+    del tram_lines[-1]  # TODO this could possibly delete the last line if file doesn't have empty space at the end 
 
     line_dict = dict()
     for line in tram_lines:
+        line = line.strip() # Removes any leading spaces, see line 10 in tramlines.txt
+        # TODO line below only works for lines with one digit (0-9) need to handle bigger numbers as well
         line_dict[line[0]] = return_stops_for_line(line)
 
     # print(line_dict["13"])
@@ -38,7 +40,7 @@ def return_stops_for_line(line):
 
 # Temporary solution, since PyCharm file system is a struggle
 def create_tram_stops():
-    with open("../data/tramstops.json", 'r') as file:
+    with open("tramstops.json", 'r') as file:
         build_tram_stops(file)
     # try:
     #     with open("data/tramstops.json", 'r') as file:
@@ -49,7 +51,7 @@ def create_tram_stops():
 
 
 def create_tram_lines():
-    with open("../data/tramstops.json", 'r') as file:
+    with open("tramlines.txt", 'r') as file:
         build_tram_lines(file)
     # try:
     #     with open("data/tramlines.txt", 'r') as file:
@@ -57,6 +59,5 @@ def create_tram_lines():
     # except FileNotFoundError:
     #     with open("C:/Users/Milvi/PycharmProjects/laboration-1/data/tramlines.txt", 'r') as file:
     #         build_tram_lines(file)
-
 
 create_tram_lines()
