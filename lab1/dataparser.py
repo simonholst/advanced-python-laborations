3a6ed9c5f42e8b7af92ec4206eed402a1fe01ab9
Simon Holst <simonhol@student.chalmers.se>
2021-11-03 15:11:05 +0100

    Update dataparser.py
    Building dictionaries for lines

--- lab1/dataparser.py
+++ lab1/dataparser.py
@@ -9,14 +9,50 @@ def build_tram_stops(jsonobject):
             'lat': unparsed_tram_stops[stop]["position"][0],
             'lon': unparsed_tram_stops[stop]["position"][1]
         }
-
     return stop_dictionary
 
 
+def build_tram_lines(lines):
+    tram_lines = lines.read()
+    tram_lines = tram_lines.split("\n\n")
+    del tram_lines[-1]
+
+    line_dict = dict()
+    for line in tram_lines:
+        line_dict[line[0]] = return_stops_for_line(line)
+
+    # print(line_dict["13"])
+
+
+def return_stops_for_line(line):
+    stations = line.split("\n")
+    # remove first element, since it is the key value for the line
+    del stations[0]
+    stops = []
+    for station in stations:
+        elements = str(station).split("  ")
+        stops.append(elements[0])
+
+    return stops
+
+
 # Temporary solution, since PyCharm file system is a struggle
-try:
-    with open("data/tramstops.json", 'r') as file:
-        build_tram_stops(file)
-except FileNotFoundError:
-    with open("C:/Users/Milvi/PycharmProjects/laboration-1/data/tramstops.json", 'r') as file:
-        build_tram_stops(file)
+def create_tram_stops():
+    try:
+        with open("data/tramstops.json", 'r') as file:
+            build_tram_stops(file)
+    except FileNotFoundError:
+        with open("C:/Users/Milvi/PycharmProjects/laboration-1/data/tramstops.json", 'r') as file:
+            build_tram_stops(file)
+
+
+def create_tram_lines():
+    try:
+        with open("data/tramlines.txt", 'r') as file:
+            build_tram_lines(file)
+    except FileNotFoundError:
+        with open("C:/Users/Milvi/PycharmProjects/laboration-1/data/tramlines.txt", 'r') as file:
+            build_tram_lines(file)
+
+
+create_tram_lines()
