9e9b9a75118e8adbe6fa9c92127f135dfb55983d
Johan Selin <johanselin99@gmail.com>
2021-11-23 13:27:24 +0100

    Test for weighted graphs

---
+++ lab2/test_weighted_graphs.py
@@ -0,0 +1,27 @@
+import unittest
+
+from lab2.graphs import WeightedGraph
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.graph = WeightedGraph()
+        self.graph.add_edge(0, 1)
+        self.graph.add_edge(0, 4)
+        self.graph.add_edge(1, 2)
+        self.graph.add_edge(1, 3)
+        self.graph.add_edge(1, 4)
+        self.graph.add_edge(2, 3)
+        self.graph.add_edge(3, 4)
+        self.graph.add_edge(3, 3)
+        self.graph.add_edge(5, 7, 2)
+
+    def test_something(self):
+        self.graph.set_weight(0, 1, 8)
+        print(self.graph.get_weight(0, 1))
+        print(self.graph.get_weight(1, 0))
+
+
+if __name__ == '__main__':
+    unittest.main()
