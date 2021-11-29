ee4f346274e93aa2ee33ef65635f61d08c707288
Simon Holst <simonhol@student.chalmers.se>
2021-11-29 11:48:41 +0100

    Refactored visualization format, in order to save graph correctly

--- lab2/visualization.py
+++ lab2/visualization.py
@@ -2,28 +2,30 @@ import graphviz
 from graphs import Graph, dijkstra
 
 
-def visualize(G, view, nodecolor):
-    pass
+def visualize(G, view='dot', name='mygraph', nodecolor=None):
+    VG = graphviz.Graph('G', filename=name, engine=view, directory='graphs')
+    for v in G.vertices():
+        v = str(v)
+        if v in nodecolor:
+            VG.node(v, style='filled', fillcolor=nodecolor[v])
+        else:
+            VG.node(v)
+    for v, w in G.edges():
+        VG.edge(str(v), str(w))
+    VG.render(format='png', view=True)
 
 
 def view_shortest(G, source, target, cost=lambda u, v: 1):
-    path = dijkstra(G, source, target, cost)
+    path = dijkstra(G, source, target, cost)[target]
     print(path)
     color_map = {str(v): 'orange' for v in path}
     print(color_map)
-    visualize(G, view='view', nodecolor=color_map)
+    visualize(G, view='sfdp', nodecolor=color_map)
 
 
 def demo():
     G = Graph([(1, 2), (1, 3), (1, 4), (3, 4), (3, 5), (3, 6), (3, 7), (6, 7)])
-    VG = graphviz.Graph('G', filename='process.gv', engine='sfdp')
-    for v in G.vertices():
-        VG.node(str(v))
-    for v, w in G.edges():
-        VG.edge(str(v), str(w))
     view_shortest(G, 2, 6)
-    VG.save()
-    VG.render(format='png', view=True)
 
 
 if __name__ == '__main__':
